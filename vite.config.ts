c13411e3cf721d5d0a09a6d6a65666ee051828e5
jnc-34 <javiercuneol@hotmail.com>
2025-11-13 17:41:36 -0300

    Fix: Inyectada VITE_API_KEY a través de la propiedad 'define' en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 🟢 FORZAMOS LA RUTA BASE a la subcarpeta de GitHub Pages
-  base: '/BANDEJITO-2.0/' 
+  
+  // 1. Corregimos la ruta base para que apunte al repositorio:
+  base: '/BANDEJITO-2.0/', 
+  
+  // 2. Definimos las variables de entorno que Vite debe inyectar en el código:
+  define: {
+    'process.env.VITE_API_KEY': JSON.stringify(process.env.VITE_API_KEY),
+  }
 });
