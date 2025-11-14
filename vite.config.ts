da71135857ab7c6dcc7d709a22b9b92717da430f
jnc-34 <javiercuneol@hotmail.com>
2025-11-14 17:59:43 -0300

    Fix: Definido index.html como input para Rollup, solucionando el error de build

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   
   // Mantenemos solo la ruta base para GitHub Pages
   base: '/BANDEJITO-2.0/', 
+  
+  // CORRECCIÓN: Le decimos a Rollup/Vite que el punto de entrada es el archivo HTML,
+  // y él encontrará el script 'index.js' referenciado dentro de él.
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
 });
