4103a1c833c181d2e4d1e276b7a8653d83c55df1
jnc-34 <javiercuneol@hotmail.com>
2025-11-14 17:49:21 -0300

    Fix: Configurado Rollup.input en vite.config.ts para resolver error de build

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,13 @@ export default defineConfig({
   
   // Mantenemos solo la ruta base para GitHub Pages
   base: '/BANDEJITO-2.0/', 
+
+  // 🟢 CORRECCIÓN: Decirle explícitamente a Rollup cuál es el archivo de entrada.
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.js', // El nuevo nombre del archivo fuente
+      },
+    },
+  },
 });
