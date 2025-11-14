06ac48c4d0e867ea3c4c520c5ac6f96e1ce0b5dc
jnc-34 <javiercuneol@hotmail.com>
2025-11-14 16:56:23 -0300

    Fix: Agregada importacion de index.css en index.tsx para cargar estilos Tailwind

--- index.tsx
+++ index.tsx
@@ -1,16 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// 🟢 CORRECCIÓN: DEBES IMPORTAR EL ARCHIVO CSS DE ENTRADA
+import './index.css'; 
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+    throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
